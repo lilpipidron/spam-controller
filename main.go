ba1cc4ffa6b1d4bc07958a22387523081023a018
lilpipidron <lociX55Lim@gmail.com>
2024-03-23 21:01:29 +0300

    feat: main

--- main.go
+++ main.go
@@ -2,9 +2,52 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"task/configs"
+	"task/db"
+	"task/internal/flood_control"
 )
 
 func main() {
+	cnf, err := configs.InitConfig("configs.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr := cnf.Connect.Host + ":" + cnf.Connect.Port
+	opt := redis.Options{
+		Addr:     addr,
+		Password: cnf.Connect.Password,
+		DB:       cnf.Connect.Database,
+		Protocol: cnf.Connect.Protocol,
+	}
+
+	client, err := db.NewClient(&opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	fch := flood_control.NewFloodControlHandler(client, cnf.Interval, cnf.Limit)
+
+	//failed flood control
+	for i := 0; i < 12; i++ {
+		client.AddNewMessage(ctx, 1, cnf.Interval)
+	}
+	control, _ := fch.Check(ctx, 1)
+	if control == false {
+		fmt.Printf("User %d failed\n", 1)
+	}
+
+	//pass flood control
+	for i := 0; i < 3; i++ {
+		client.AddNewMessage(ctx, 2, cnf.Interval)
+	}
+	control, _ = fch.Check(ctx, 2)
+	if control == true {
+		fmt.Printf("User %d pass\n", 2)
+	}
 
 }
 
